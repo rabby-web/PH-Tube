fe7de3a6c5d7d294261f3c651a9bf1bededc333b
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-31 20:32:13 +0600

    grid card add

--- js/inde.js
+++ js/inde.js
@@ -9,8 +9,8 @@ const handleCategory = async () => {
     const categoryContainer = document.getElementById("category-container");
     const p = document.createElement("p");
     p.innerHTML = `
-    <p
-            class="bg-slate-500 mx-2 py-2 px-4 rounded text-white text-lg font-semibold"
+    <p onclick="handleLoadData('${category.category_id}')" 
+             class="bg-slate-500 mx-2 py-2 px-4 rounded text-white text-lg font-semibold"
           >
             ${category.category}
           </p>
@@ -18,4 +18,16 @@ const handleCategory = async () => {
     categoryContainer.appendChild(p);
   });
 };
+const handleLoadData = async (id) => {
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${id}`
+  );
+  const categoryLoadData = await response.json();
+  const categoryShowCard = categoryLoadData.data;
+  console.log(categoryShowCard);
+  categoryShowCard.forEach((category) => {
+    console.log(category);
+  });
+};
+handleLoadData();
 handleCategory();
