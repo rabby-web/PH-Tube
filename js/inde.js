fcab51a51cb1e2ff1585fbed0dec1e95bd0393c0
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-01 23:06:32 +0600

    renew

--- js/inde.js
+++ js/inde.js
@@ -1,83 +1,116 @@
-const handleCategory = async () => {
-  const response = await fetch(
+let menuTrimGlobal = [];
+
+const loadMenuData = async () => {
+  const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
   );
-  const data = await response.json();
-  const categoryData = data.data;
-  // console.log(categoryData);
-  categoryData.forEach((category) => {
-    const categoryContainer = document.getElementById("category-container");
-    const p = document.createElement("p");
-    p.innerHTML = `
-    <p onclick="handleLoadData('${category.category_id}')" 
-             class="bg-slate-300 mx-2 py-2 px-4 rounded text-lg font-semibold"
-          >
-            ${category.category}
-          </p>
-    `;
-    categoryContainer.appendChild(p);
+  const data = await res.json();
+
+  const menuTrimData = data.data;
+
+  displayMenuData(menuTrimData);
+
+  menuTrimGlobal = data;
+};
+
+const displayMenuData = (menuTrimData) => {
+  const menuContainer = document.getElementById("category-container");
+
+  menuTrimData.forEach((singleMenu) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+            <button onclick="displayCardData('${singleMenu.category_id}') ; sortFunc('${singleMenu.category_id}') ; " class="btn btn-sm">${singleMenu.category}</button>
+        `;
+    menuContainer.appendChild(div);
+
+    displayCardData("1000");
   });
 };
-const handleLoadData = async (id) => {
-  const response = await fetch(
-    `https://openapi.programming-hero.com/api/videos/category/${id}`
-  );
-  const categoryLoadData = await response.json();
-  const categoryCard = categoryLoadData.data;
 
-  const empty = document.getElementById("empty-container");
-  if (!categoryCard[0]) {
-    empty.classList.remove("hidden");
-  } else {
-    empty.classList.add("hidden");
-  }
-  console.log(categoryCard);
+const displayCardData = async (categoryId, sotto = false) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await res.json();
+
+  const trimCard = data.data;
+
   const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = " ";
-  categoryCard.forEach((c) => {
-    console.log(c.id);
-    // console.log(c?.thumbnail);
-    // console.log(c?.authors[0]?.profile_picture);
-    // console.log(c?.authors[0]?.verified);
-    // console.log(c?.others);
+
+  cardContainer.innerHTML = "";
+
+  const empltyContainer = document.getElementById("empty-container");
+
+  if (!trimCard[0]) {
+    empltyContainer.classList.remove("hidden");
+  } else {
+    empltyContainer.classList.add("hidden");
+  }
+
+  if (sotto == true) {
+    trimCard.sort((a, b) => {
+      return parseFloat(b.others.views) - parseFloat(a.others.views);
+    });
+  }
+
+  trimCard.forEach((singleCard) => {
     const div = document.createElement("div");
-    const minutesAll = c.others.posted_date / 60;
-    const hours = minutesAll / 60;
-    const minutes = minutesAll % 60;
-    div.classList = `card w-full bg-base-100 shadow-xl`;
+
+    const allMinute = singleCard.others.posted_date / 60;
+    const allHours = allMinute / 60;
+    const sngleMinutes = allMinute % 60;
+
     div.innerHTML = `
-    <figure>
-    <div class="relative">
-    <img class="w-full h-64 rounded-lg" src="${c.thumbnail}" alt="Shoes" />
-    <p class="bg-[#171717] absolute right-4 bottom-3 rounded-md p-1 text-[12px] text-white ${
-      c.others.posted_date !== "" ? "" : "hidden"
-    }">
-                        ${Math.trunc(hours)} hrs ${Math.trunc(minutes)} minute
-                    </p>
-    </div>
-    </figure>
-    <div class="div p-3">
-     <div class="flex">
-        <div class="w-10 rounded-full m-2">
-            <img class="rounded-full w-10 h-10" src="${
-              c?.authors[0]?.profile_picture
-            }" />
-        </div>
-        <div class="space-y-2">
-            <h2 class="text-2xl font-bold mt-1">${c.title}</h2>
-            <p class="text-lg items-center font-semibold flex"><small class="pr-1">${
-              c?.authors[0]?.profile_name
-            }</small>  <small> ${
-      c?.authors[0]?.verified == true ? "<img src='../image/svg-icon.svg'>" : ""
-    }</small></p>
-            <p class="text-sm">${c.category_id} views</p>
-        </div>
-     </div>
-    `;
+            
+            <div class="max-w-86">
+                <figure class="relative">
+                <img class="rounded-lg h-48 w-full" src="${
+                  singleCard.thumbnail
+                }" alt="Shoes"/>
+                <p class="bg-[#171717] absolute right-4 bottom-3 rounded-md p-1 text-[10px] text-white ${
+                  singleCard.others.posted_date !== "" ? "" : "hidden"
+                }">
+                ${Math.trunc(allHours)} hrs ${Math.trunc(sngleMinutes)} minute
+            </p>
+                </figure>
+                <div class="p-4 flex gap-2">
+
+                    <img class="w-9 h-9 rounded-full"
+                        src="${singleCard?.authors[0]?.profile_picture}"
+                        alt="">
+
+                    <div>
+                        <h2 class="text-md font-bold">${singleCard.title}</h2>
+                        <div class="flex gap-2 items-center mt-3">
+                            <p class="text-md">${
+                              singleCard?.authors[0]?.profile_name
+                            }</p>
+                            <small> ${
+                              singleCard?.authors[0]?.verified == true
+                                ? "<img src='./images/verified.png'>"
+                                : ""
+                            }</small>
+                        </div>
+                        <p class="text-md">${
+                          singleCard?.others?.views
+                        } views</p>
+                    </div>
+
+
+                </div>
+            </div>
+        `;
+
     cardContainer.appendChild(div);
   });
 };
-// const handleEmptyData =
-handleLoadData("1000");
-// handleLoadData();
-handleCategory();
+
+loadMenuData();
+
+const sortFunc = (id) => {
+  const btnContainer = document.getElementById("sort-button-wrapper");
+  btnContainer.textContent = "";
+  btnContainer.innerHTML = `
+        <button onclick="displayCardData('${id}', true)" class="btn btn-sm md:btn-md">Sort by view</button>
+    `;
+};
